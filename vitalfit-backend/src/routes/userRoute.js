03729cc0d96ef4ac8a2fae3f40d686507b4ea788
jamppohj <jamppohj@gmail.com>
2025-08-07 17:36:50 +0900

    userRoute.js

--- vitalfit-backend/src/routes/userRoute.js
+++ vitalfit-backend/src/routes/userRoute.js
@@ -2,15 +2,8 @@ const express = require('express');
 const path = require('path');
 const router = express.Router();
 const userController = require('../controllers/userController');
-const { createUpload } = require('../utils/uploadUtils');
+const { uploadSingle, handleError, processFile } = require('../middlewares/profileUpload');
 const auth = require('../middlewares/authMiddleware');
-
-// uploadUtils.js를 사용한 프로필 업로드 미들웨어 생성
-const { uploadSingle, handleError, processFile } = createUpload('profiles', 'profile_image', 5, [
-  '.jpg',
-  '.jpeg',
-  '.png',
-]);
 
 router.post('/signup', uploadSingle, handleError, processFile, userController.signUp);
 router.post('/signin', userController.signIn);
