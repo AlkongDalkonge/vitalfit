64603d14d5dea26b5b49595d2418d91ca7ed46fd
jamppohj <jamppohj@gmail.com>
2025-08-13 15:22:23 +0900

    azure DB

--- vitalfit-backend/src/config/config.js
+++ vitalfit-backend/src/config/config.js
@@ -3,7 +3,7 @@ console.log('Database configuration loaded');
 
 module.exports = {
   development: {
-    username: process.env.DB_USERNAME || 'aldalkong',
+    username: process.env.DB_USERNAME || 'postgres',
     password: process.env.DB_PASSWORD || 'postgres',
     database: process.env.DB_NAME || 'vitalfit',
     host: process.env.DB_HOST || 'localhost',
